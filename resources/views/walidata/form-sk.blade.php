81e252c6d79271d47e3a9ab4dfffc5fde1e52d44
Nugroho Bin Purboyuwono <nugroho.redbuff@gmail.com>
2026-08-18 10:35:48 +0700

    form sk

--- resources/views/walidata/form-sk.blade.php
+++ resources/views/walidata/form-sk.blade.php
@@ -16,7 +16,7 @@
     </section>
 
     <div class="section bg-white rounded-3 p-3 mb-2">
-        <form action="#" method="post">
+        <form action="#" method="post" id="skform">
             {{-- Imput CCD_Descs --}}
             <h5>Deskripsi Sub Kegiatan</h5>
             <table class="table table-sm mb-2">
@@ -58,12 +58,54 @@
                         <td colspan="5"><textarea id="indikator" rows="3" class="form-control"></textarea></td>
                     </tr>
                     <tr>
-                        <td colspan="2">Satuan</td>
-                        <td colspan="3">Baseline</td>
+                        <td colspan="5">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label for="satuan">Satuan</label>
+                                    <input type="text" class="form-control" id="satuan">
+                                </div>
+                                <div class="col-sm-6">
+                                    <label for="satuan">Baseline</label>
+                                    <input type="number" id="baseline" class="form-control" min="0" value="0">
+                                </div>
+                            </div>
+                        </td>
                     </tr>
                     <tr>
-                        <td colspan="2"><input type="text" class="form-control" id="satuan"></td>
-                        <td colspan="3"><input type="number" id="baseline" class="form-control" min="0" value="0"></td>
+                        <td colspan="5">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <label for="iku_tipehitung">Tipe Perhitungan</label>
+                                    <input type="text" id="iku_tipehitung" class="form-control">
+                                </div>
+                                <div class="col-sm-4">
+                                    <label for="iku_sumberdata">Sumber Data</label>
+                                    <input type="text" id="iku_sumberdata" class="form-control">
+                                </div>
+                                <div class="col-sm-4">
+                                    <label for="iku_penjab">Penanggung Jawab</label>
+                                    <input type="text" id="iku_penjab" class="form-control">
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="5">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <label for="iku_alasan">Alasan Pemilihan Indikator</label>
+                                    <textarea id="iku_alasan" rows="3" class="form-control"></textarea>
+                                </div>
+                                <div class="col-sm-4">
+                                    <label for="iku_do">Definisi Operasional</label>
+                                    <textarea id="iku_do" rows="3" class="form-control"></textarea>
+                                </div>
+                                <div class="col-sm-4">
+                                    <label for="iku_formulasi">Formulasi</label>
+                                    <textarea id="iku_formulasi" rows="3" class="form-control"></textarea>
+                                </div>
+                            </div>
+                        </td>
                     </tr>
                     <tr>
                         <td colspan="5" class='text-start'>Target Kinerja Per Tahun</td>
@@ -126,7 +168,7 @@
 
 @section('scriptes')
     <script>
-        const kegs=[];
+        let kegs=[];
         $(function(){
             fetch(apiurl + "/desc/list-kegiatan")
             .then(response=>response.json())
@@ -143,7 +185,7 @@
             $("#paramaksi").keypress(function(ev){
                 if(ev.which === 13){
                     ev.preventDefault();
-                    let aksi=kegs.filter(item => item.deskripsi.startsWith('Pengelolaan'));
+                    let aksi=kegs.filter(item => item.deskripsi.startsWith($(this).val()));
                     $("#aksidata").empty();
                     aksi.forEach(item=>{
                         $("#aksidata").append(`
@@ -156,5 +198,96 @@
                 }
             })
         })
+
+        var skmax="";
+        async function getSkmax(master_id) {
+            try {
+                const response = await fetch(apiurl + "/desc/skmax/" + master_id);
+                const data = await response.json();
+                let sk_id = data.data[0].sk_id;
+                console.log("sk id:",typeof sk_id);
+                let skmax = sk_id.toString().padStart(2, '0');
+                // console.log(skmax);
+                return skmax;
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+
+        $("#aksidata").on('click','.master_id',function(){
+            let mid = $(this).text();
+            getSkmax(mid).then(skmaxv => {
+                // Gunakan skmax di sini
+                // skmax = skmaxv;
+                // console.log(skmax);
+                // master_id = replace 00 terakhir dengan skmax
+                let master_id = mid.slice(0,-2)+skmaxv;
+                let id=mid.split("-");
+                $("#master_id").val(master_id);
+                $("#tj_id").val(id[0]);
+                $("#ss_id").val(id[1]);
+                $("#pg_id").val(id[2]);
+                $("#kg_id").val(id[3]);
+                $("#sk_id").val(skmaxv);
+            });
+        })
+
+        
+        // form submit
+        $("#skform").on('submit',function(ev){
+            ev.preventDefault();
+            const descids=["master_id","tj_id","ss_id","pg_id","kg_id","sk_id","deskripsi_1"];
+            const indiids=['master_id','indikator','satuan','baseline','kt1','kt2','kt3','kt4','kt5','iku_alasan','iku_tipehitung','iku_formulasi','iku_do','iku_penjab','iku_sumberdata'];
+            const budgids=['master_id','at1','at2','at3','at4','at5'];
+
+            // collect descdata
+            let descdata = {};
+            descids.forEach(item=>{
+                descdata[item]=$("#"+item).val();
+            })
+
+            let indidata = {};
+            indiids.forEach(item=>{
+                if(item == 'kt1'){
+                    indidata['t1']=$("#"+item).val();
+                }else if(item == 'kt2'){
+                    indidata['t2']=$("#"+item).val();
+                }else if(item == 'kt3'){
+                    indidata['t3']=$("#"+item).val();
+                }else if(item == 'kt4'){
+                    indidata['t4']=$("#"+item).val();
+                }else if(item == 'kt5'){
+                    indidata['t5']=$("#"+item).val();
+                }else if(item == 'master_id'){
+                    indidata['master_ik']=$("#master_id").val()+"-01";
+                }else{
+                    indidata[item]=$("#"+item).val();
+                }                
+            })
+
+            let budgdata = {};
+            budgids.forEach(item=>{
+                if(item == 'at1'){
+                    budgdata['t1']=$("#"+item).val();
+                }else if(item == 'at2'){
+                    budgdata['t2']=$("#"+item).val();
+                }else if(item == 'at3'){
+                    budgdata['t3']=$("#"+item).val();
+                }else if(item == 'at4'){
+                    budgdata['t4']=$("#"+item).val();
+                }else if(item == 'at5'){
+                    budgdata['t5']=$("#"+item).val();
+                }else if(item == 'master_id'){
+                    budgdata['master_ik']=$("#master_id").val()+"-01";
+                }else{
+                    budgdata[item]=$("#"+item).val();
+                }                
+            })
+
+            // console.log(descdata);
+            let data = {"deskripsi":descdata,"indikator":indidata,"budget":budgdata};
+            console.log(data);
+        })
+
     </script>
 @endsection
